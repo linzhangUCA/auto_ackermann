2790a47f1d42b85af29ad45ad0ce0c6efc04cc84
Lin Zhang <linzhank@gmail.com>
2023-01-04 23:13:12 -0600

    add joystick number assertion

--- vehicle.py
+++ vehicle.py
@@ -9,24 +9,30 @@ from gpiozero import PhaseEnableMotor
 
 
 class Vehicle:
-
-    def __init__(self, servo_address=0x40, servo_channel=0, motor_phase=24, motor_enable=18):
-        # instantiate motor and servo
-        self.engine = PhaseEnableMotor(phase=motor_phase, enable=motor_enable)
-        self.servo_kit = ServoKit(channels=16, address=servo_address)
-        self.steer = servo_kit.servo[servo_channel]
-        # set limit for motor and servo
-        self.MAX_THROTTLE = 0.25
-        assert MAX_THROTTLE <= 1
-        self.STEER_CENTER = 100
-        self.MAX_STEER = 50
-        self.steer.angle = self.STEER_CENTER
+    def __init__(
+        self, kit_address=0x40, servo_channel=0, motor_phase=24, motor_enable=18
+    ):
+        # vehicle has to be under control
         display.init()
         joystick.init()
-        print(f"{self.get_numControllers()} joystick connected")
+        print(f"{joystick.get_count()} joystick connected")
+        assert joystick.get_count() == 1, f"Vehicle has to be controlled by exact 1 joystick"
         self.controller = joystick.Joystick(0)
+        # instantiate motor and servo
+        self.kit = ServoKit(channels=16, address=kit_address)
+        self.rudder = self.kit.servo[servo_channel]
+        self.engine = PhaseEnableMotor(phase=motor_phase, enable=motor_enable)
+        # set limit for motor and servo
+        self.MAX_THROTTLE = 0.25
+        assert self.MAX_THROTTLE <= 1
+        self.STEER_CENTER = 100
+        self.MAX_STEER = 50
+        self.rudder.angle = self.STEER_CENTER
 
-    def get_numControllers():
-        return joystick.get_count()
+    def halt(self):
+        self.engine.stop()
+        self.engine.close()
 
 
+# test
+bot = Vehicle()
