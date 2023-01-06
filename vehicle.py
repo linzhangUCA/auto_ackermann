9da759952a58ddfae8884657e5d25314693f9a1a
Lin Zhang <linzhank@gmail.com>
2023-01-05 22:42:21 -0600

    update vehicle.py

--- vehicle.py
+++ vehicle.py
@@ -23,16 +23,39 @@ class Vehicle:
         self.rudder = self.kit.servo[servo_channel]
         self.engine = PhaseEnableMotor(phase=motor_phase, enable=motor_enable)
         # set limit for motor and servo
-        self.MAX_THROTTLE = 0.25
-        assert self.MAX_THROTTLE <= 1
-        self.STEER_CENTER = 100
-        self.MAX_STEER = 50
-        self.rudder.angle = self.STEER_CENTER
+        self.max_throttle = 0.25
+        assert self.max_throttle <= 1
+        self.steer_center = 100
+        self.max_steer = 50
+        self.rudder.angle = self.steer_center
 
     def halt(self):
         self.engine.stop()
         self.engine.close()
+        sys.exit()
 
 
-# test
-bot = Vehicle()
+# Uncomment following to test
+# bot = Vehicle()
+# try:
+#     while True:
+#         for e in event.get():
+#             if e.type == QUIT:
+#                 print("QUIT detected, terminating...")
+#                 pygame.quit()
+#                 sys.exit()
+#             if e.type == JOYAXISMOTION:
+#                 val_0 = bot.controller.get_axis(0)
+#                 val_4 = bot.controller.get_axis(4)
+#                 print(f"steering joystick value: {val_0}, speed joystick value: {val_4}")
+#                 speed = -np.clip(val_4, -bot.max_throttle, bot.max_throttle)
+#                 if speed > 0:
+#                     bot.engine.forward(speed)
+#                 elif speed < 0:
+#                     bot.engine.backward(-speed)
+#                 else:
+#                     bot.engine.stop()
+#                 ang = bot.steer_center - bot.max_steer * val_0
+#                 bot.rudder.angle = ang
+# except KeyboardInterrupt:
+#     bot.halt()
